dd0c60ae0ab03a030fca3adc39b5de9827eabda8
Gleywson <Gleywson@200.17.75.102>
2018-01-16 13:50:05 -0300

    adição da classe REst

--- src/main/java/br/com/devmedia/jmcontroleprojeto/rest/PopulaBancoRest.java
+++ src/main/java/br/com/devmedia/jmcontroleprojeto/rest/PopulaBancoRest.java
@@ -5,10 +5,64 @@
  */
 package br.com.devmedia.jmcontroleprojeto.rest;
 
+import br.com.devmedia.jmcontroleprojeto.entidades.Estado;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.Query;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author Gleywson
  */
+@Stateless
+@Path(value = "/populaBancoRest")
 public class PopulaBancoRest {
-    
+
+    @PersistenceContext(unitName = "ControleProjetoPU", type = PersistenceContextType.TRANSACTION)
+    private EntityManager em;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response popularTabelasEstadosCidades() {
+
+        Query query = em.createNamedQuery("Estado.findAll");
+
+        List<Estado> estados = query.getResultList();
+
+        if (estados.isEmpty()) {
+            popularBanco();
+            estados = query.getResultList();
+        }
+
+        return Response.ok().build();
+    }
+
+    private void popularBanco() {
+
+        Estado saoPaulo = new Estado();
+        saoPaulo.setNome("Sao Paulo");
+        em.persist(saoPaulo);
+
+        Estado minasGerais = new Estado();
+        minasGerais.setNome("Minas Gerais");
+        em.persist(minasGerais);
+
+        Estado rioJaneiro = new Estado();
+        rioJaneiro.setNome("Rio de Janeiro");
+        em.persist(rioJaneiro);
+
+        criarCidades();
+    }
+
+    private void criarCidades() {
+        //Código para criar as cidades no banco de dados
+    }
 }
